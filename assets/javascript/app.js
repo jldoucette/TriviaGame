53b41a5478d737fe3a475bb6a999cca34bd14b83
jldoucette <jd@jonathandoucette.com>
2017-04-21 21:10:56 -0700

    Friday later in progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -335,33 +335,69 @@ $(document).ready(function() {
 
 	gameQuestions();
 });
-// function getAnswer() {
-// 			answered=false;
-// 			questionTimerStart();
 
-// 		$("#answers").click(function(event){
-			
-// 			var clickedValue=$(event.target).text();
-// 			console.log("Clicked " + clickedValue);
-// 			 $("#answers").off("click");
-// 			if (clickedValue==arrRoundAnswerOptions[correctAnswerIndex]) {
-				
-// 				console.log("Correct Answer!!! " + clickedValue + arrRoundAnswerOptions[correctAnswerIndex]);
-// 				questionTimerStop();
-// 				answered=true;
-// 				correct++;
-// 				console.log("Correct Answers: " + correct);
-// 				}
+var intervalId;
 
-// 			else {
-// 				console.log("WRONG Answer!!! " + clickedValue + arrRoundAnswerOptions[correctAnswerIndex]);
-// 				questionTimerStop();
-// 				answered==true;
-// 				incorrect++;
-// 				console.log("Incorrect Answer: " + incorrect);
-// 				}
-// 			return;
-// 			});
+//  Our stopwatch object.
+var timer = {
+
+  time: 10,
+
+
+  reset: function() {
+
+    timer.time = 0;
+
+    $("#timerdisplay").html("00:10");
+    //  TODO: Change the "display" div to "00:00."
+ 
+
+  },
+
+  start: function() {
+
+    //  TODO: Use setInterval to start the count here.
+    intervalId=setInterval(timer.count,-1000);
+  
+  },
+
+  
+  count: function() {
+    timer.time--;
+    var timeResult=timer.timeConverter(timer.time);
+    $("#timerdisplay").html(timeResult);
+    //  TODO: increment time by 1, remember we cant use "this" here.
+
+    //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
+    //        and save the result in a variable.
+
+    //  TODO: Use the variable you just created to show the converted time in the "display" div.
+
+  },
+
+
+
+  timeConverter: function(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+}
+
 
 
 
